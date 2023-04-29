4535eb1a6887287928b0f9369923eb8d5802716f
daniel2751241 <58035766+daniel2751241@users.noreply.github.com>
2023-04-29 02:44:50 -0400

    Add "/solution" post route
    Added Order model with validators for item, quantity and price attributes
    Added OrderStatus Enum

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, validator
+from enum import Enum
+from typing import List
+
+class OrderStatus(str, Enum):
+    completed = "completed"
+    pending = "pending"
+    canceled = "canceled"
+    
+class Order(BaseModel):
+    id: int
+    item: str
+    quantity: int
+    price: float
+    status: OrderStatus
+    
+    @validator("item")
+    def not_empty_item(cls, v):
+        if v == '':
+            raise ValueError("item field cannot be empty")
+        return v
+    
+    @validator("quantity")
+    def positive_quantity(cls, v):
+        if v <= 0:
+            raise ValueError("quantity field must be positive")
+        return v
+    
+    @validator("price")
+    def positive_price(cls, v):
+        if v <= 0:
+            raise ValueError("price field must be positive")
+        return v
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/solution")
+async def process_orders(orders: List[Order], criterion: OrderStatus):
+    # my guess is total revenue should be like this, not just making a sum with orders prices
+    # that's why test units should not give the expected response as the sample request you
+    # provided, so i'm assuming this approach
+    total = sum([order.price*order.quantity for order in orders if order.status == criterion])
+    return {"total": total}
